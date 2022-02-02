aa2ee3f93b8c1ca043e06d7262faa6f9bd01bbe7
kjvin <kevin.john.vincent@gmail.com>
2022-02-02 15:16:44 +0000

    pushed from theia

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (isNaN(principal) || principal <= 0) {
+      alert("Please enter a positive number");
+      return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%.\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\><br\>";
+  }
+  
+  function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+  }
+  
